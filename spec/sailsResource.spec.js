85f666407345b5be37356589649233647ca11ee8
Joshua Prodahl <jprodahl@gmail.com>
2014-06-29 22:51:49 -0500

    creating mock socket class; starting a test

--- spec/sailsResource.spec.js
+++ spec/sailsResource.spec.js
@@ -4,17 +4,19 @@ describe("sailsResource", function() {
 
     beforeEach(function() {
         module("sailsResource");
-        /*inject(function(sailsResource, _$rootScope_, _$window_) {
+        inject(function(sailsResource, _$window_) {
             socket = _$window_.io.connect();
             service = sailsResource({
                 model: "widget",
                 socket: socket
             });
-        });*/
+        });
     });
 
     it("should query for a list of items", function() {
-
+        var items = service.query();
+        expect(items).toBeDefined();
+        // TODO the mockSocket should probably not return its items right away
     });
 
 });
