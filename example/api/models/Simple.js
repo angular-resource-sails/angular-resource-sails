014fda72e9a6fdae5429080afeecf0acba16de76
Jason More <jasonmore@Jasons-MacBook-Pro.local>
2014-08-14 17:05:48 -0500

    working on example app saving

---
+++ example/api/models/Simple.js
@@ -0,0 +1,23 @@
+/**
+ * Simple.js
+ *
+ * @description :: Check all the types
+ * @docs        :: http://sailsjs.org/#/documentation/concepts/ORM
+ */
+
+module.exports = {
+
+  attributes: {
+    email: 'string',
+    isAwesome: 'boolean',
+    manyThings: 'array',
+    oneTwoOrThree: {
+      type: 'string',
+      enum: ['one', 'two', 'three']
+    },
+    perfectDate: 'datettime',
+    age: 'int',
+    answerToEverything: 'float'
+  }
+};
+
--- example/assets/js/app/home/HomeController.js
+++ example/assets/js/app/home/HomeController.js
@@ -1,5 +1,20 @@
 app.controller('HomeController', function (sailsResource) {
-  this.thingsAndStuff = "wowowowo";
+  var simple = sailsResource('Simple');
 
-//  this.simpleTypes = sailsResource('simpleTypes');
+  this.simpleForm = {};
+
+  this.simpleTypes = simple.query();
+
+  this.addClassicNgResource = function () {
+    var newSimple = new simple(this.simpleForm);
+    newSimple.$save();
+
+    this.simpleForm = {};
+  };
+
+  this.addJproWay = function(){
+    var newSimple = new simple();
+    angular.copy(this.simpleForm, newSimple);
+    newSimple.$save();
+  };
 });
