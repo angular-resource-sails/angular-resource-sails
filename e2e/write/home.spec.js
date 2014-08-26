7c9d48aad7d9b8bc93e4acba78c47ccc5640d708
Jason More <jasonmore@Jasons-MacBook-Pro.local>
2014-08-26 08:11:15 -0500

    refactored protractor bindings to match actual angular page bindings

--- e2e/write/home.spec.js
+++ e2e/write/home.spec.js
@@ -1,32 +1,36 @@
-var Homepage = function() {
-	this.get = function() {
+var Homepage = function () {
+	this.get = function () {
 		browser.get('http://localhost:1337');
 	};
 
-	this.simples = element.all(by.repeater('simple in home.simpleTypes track by simple.id'));
+	this.simpleTypes = element.all(by.repeater('simple in home.simpleTypes track by simple.id'));
 
-	this.email = element(by.model('home.simpleForm.email'));
-	this.twoRadio = element.all(by.model('home.simpleForm.oneTwoOrThree')).get(1);
-	this.manyThings = element(by.model('home.simpleForm.manyThings'));
-	this.submit = element(by.css('button[type=submit]'));
+	this.simpleForm = {
+		email: element(by.model('home.simpleForm.email')),
+		twoRadio: element.all(by.model('home.simpleForm.oneTwoOrThree')).get(1),
+		manyThingsTwo: element(by.model('home.simpleForm.manyThings')),
+		submit: element(by.css('button[type=submit]'))
+	};
+
+
 };
 
 
-describe('homepage', function() {
+describe('homepage', function () {
 
 	var home = new Homepage();
 	home.get();
 
-	it('simple list should be empty', function() {
-		expect(home.simples.count()).toEqual(0);
+	it('simple list should be empty', function () {
+		expect(home.simpleTypes.count()).toEqual(0);
 	});
 
 	it('can add new simple type', function () {
-		home.email.sendKeys('Jason.More@gmail.com');
-		home.twoRadio.click();
-		home.manyThings.click();
-		home.submit.click();
+		home.simpleForm.email.sendKeys('Jason.More@gmail.com');
+		home.simpleForm.twoRadio.click();
+		home.simpleForm.manyThingsTwo.click();
+		home.simpleForm.submit.click();
 
-		expect(home.simples.count()).toEqual(1);
+		expect(home.simpleTypes.count()).toEqual(1);
 	});
 });
