faf0ea765818f78329b7951b76f90d29075289b2
Jason More <Jason.More@gmail.com>
2014-09-29 14:40:05 -0500

    fixed grunt-watch for @xjpro

--- example/tasks/config/watch.js
+++ example/tasks/config/watch.js
@@ -23,7 +23,7 @@ module.exports = function(grunt) {
 		assets: {
 
 			// Assets to watch:
-			files: ['assets/**/*', 'tasks/pipeline.js'],
+			files: ['assets/**/*', '../src/sailsResource.js', 'tasks/pipeline.js'],
 
 			// When assets are changed:
 			tasks: ['syncAssets' , 'linkAssets']
